720c39b38618af7935e044195e6aa91a1fdd70e2
efumagal <efumagal@cisco.com>
2018-07-16 22:57:51 +0100

    added deck.js layer

--- src/views/Test/Test.js
+++ src/views/Test/Test.js
@@ -1,24 +1,42 @@
 import React, { Component } from "react";
-import { Card, CardBody, CardColumns, CardHeader } from "reactstrap";
-import { CustomTooltips } from "@coreui/coreui-plugin-chartjs-custom-tooltips";
-import {defaultMapStyle, pointLayer} from './map-style.js';
+import { defaultMapStyle, pointLayer } from "./map-style.js";
 
 import ReactMapGL from "react-map-gl";
+import DeckGLOverlay from "./deckgl-overlay";
+import taxiData from "./taxi";
 
 class Test extends Component {
-  state = {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mapStyle: defaultMapStyle,
+      viewport: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        longitude: -74,
+        latitude: 40.7,
+        zoom: 11,
+        maxZoom: 16
+      }
+    };
+    this._resize = this._resize.bind(this);
+  }
+
+  /*   state = {
     mapStyle: defaultMapStyle,
     viewport: {
-      width: 400,
-      height: 400,
+      width: window.innerWidth,
+      height: window.innerHeight,
       latitude: 51.444,
       longitude: -0.350713,
-      zoom: 11
+      zoom: 11,
+      maxZoom: 16
     }
-  };
+  }; */
 
   componentDidMount() {
     window.addEventListener("resize", this._resize);
+    this._processData();
     this._resize();
   }
 
@@ -26,15 +44,42 @@ class Test extends Component {
     window.removeEventListener("resize", this._resize);
   }
 
-  _onViewportChange = viewport =>
-    this.setState({
-      viewport: { ...this.state.viewport, ...viewport }
-    });
+  _processData() {
+    if (taxiData) {
+      this.setState({ status: "LOADED" });
+      const points = taxiData.reduce((accu, curr) => {
+        accu.push({
+          position: [
+            Number(curr.pickup_longitude),
+            Number(curr.pickup_latitude)
+          ],
+          pickup: true
+        });
+        accu.push({
+          position: [
+            Number(curr.dropoff_longitude),
+            Number(curr.dropoff_latitude)
+          ],
+          pickup: false
+        });
+        return accu;
+      }, []);
+      this.setState({
+        points,
+        status: "READY"
+      });
+    }
+  }
 
   _resize = () =>
     this._onViewportChange({
-      width: this.props.width || window.innerWidth,
-      height: this.props.height || window.innerHeight
+      width: window.innerWidth,
+      height: window.innerHeight
+    });
+
+  _onViewportChange = viewport =>
+    this.setState({
+      viewport: { ...this.state.viewport, ...viewport }
     });
 
   render() {
@@ -44,8 +89,13 @@ class Test extends Component {
         mapboxApiAccessToken={
           "pk.eyJ1IjoiZW1hZnVtYSIsImEiOiJjamh1ZGVoZGowbGExM3duMDkwMnhtNDhiIn0.xgW6mtfaTEgFNw8jC6i_Yw"
         }
-        onViewportChange={viewport => this.setState({ viewport })}
-      />
+        onViewportChange={viewport => this._onViewportChange(viewport)}
+      >
+        <DeckGLOverlay
+          viewport={this.state.viewport}
+          data={this.state.points}
+        />
+      </ReactMapGL>
     );
   }
 }
